2bba1174ab3723a64e78e45a17b4f4330e8c4ad1
tucuvaldez <105434510+tucuvaldez@users.noreply.github.com>
2022-11-10 14:45:50 +0100

    elimino funcion test, para que se ejecute bien.

--- test.js
+++ test.js
@@ -2,31 +2,23 @@ let express = require('express');
 const PORT = process.env.PORT || 8080;
 let app = express();
 
-
-
-
 const HandleProducts = require('./handleProducts')
 
 
-async function test() {
-    let productos = new HandleProducts('productos.txt')
+let productos = new HandleProducts('productos.txt')
+// let getData = await productos.getData();
+// let getRandom = await productos.getRandom();
+// console.log(getData);
 
+app.get('/productos', async (req, res, next) => {
+    res.send(await productos.getData());
+})
 
-    let getData = await productos.getData();
-    let getRandom = await productos.getRandom();
-    console.log(getData);
-
-    app.get('/productos', (req, res, next) => {
-        res.send(`${JSON.stringify(getData)}`);
-    })
-
-    app.get('/productoRandom', (req, res, next) => {
-        res.send(`${JSON.stringify(getRandom)}`);
-    })
-
-};
+app.get('/productoRandom', async (req, res, next) => {
+    res.send(await productos.getRandom());
+})
 
 
 
 app.listen(PORT, () => { console.log(`Server on http://localhost:${PORT}/productos`) });
-test();
+
